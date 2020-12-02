5e65f2051ff4cb7c0ffd2a5267f993c56821174e
Paul Glavich <glav@theglavs.com>
2020-12-03 09:41:03 +1100

    positioning stuff

---
+++ Assets/_app/Scripts/MapManager.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Maps.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField] MapSession mapSession;
+
+    private void OnEnable()
+    {
+        Debug.Log("Initialising BING SDK Key");
+        var keys = BingKeys.LoadInstance();
+        if (string.IsNullOrWhiteSpace(keys.bingMapSdkKey))
+        {
+            Debug.Log(" BING SDK Key not found.");
+            throw new System.Exception("No Key!");
+        }
+        mapSession.DeveloperKey = keys.bingMapSdkKey;
+        Debug.Log("Done initialising BING SDK Key");
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
