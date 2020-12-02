c7489d416b997dad2f5385ccb7682906cc871c78
Paul Glavich <glav@theglavs.com>
2020-12-02 16:53:39 +1100

    Addition of a BingKeys SDK key

---
+++ Assets/_app/Scripts/BingKeys.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "BingMap/Keys", fileName = "BingKeys")]
+public class BingKeys : ScriptableObject
+{
+    public string bingMapSdkKey;
+
+    public static BingKeys LoadInstance()
+    {
+        var secrets = Resources.Load<BingKeys>("BingKeys");
+        if (secrets == null)
+        {
+            Debug.LogError("No secrets/BinKeys defined.  Please create secrets asset in Resources/Secrets.asset");
+        }
+        else
+        {
+            Debug.Log($"Loaded secrets/keys from asset");
+        }
+
+        return secrets;
+    }
+}
