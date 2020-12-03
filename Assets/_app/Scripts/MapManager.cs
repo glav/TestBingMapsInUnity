ec67b8945e0da2806d8b33045ba3c51aa4b3f83d
Paul Glavich <glav@theglavs.com>
2020-12-03 12:15:17 +1100

    Simple Unity bing maps example

--- Assets/_app/Scripts/MapManager.cs
+++ Assets/_app/Scripts/MapManager.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.Maps.Unity;
+﻿using Microsoft.Geospatial;
+using Microsoft.Maps.Unity;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
     [SerializeField] MapSession mapSession;
+    [SerializeField] MapPin mapPinPrefab;
+    private MapRenderer mapRenderer;
+    private MapPinLayer pinLayer;
+    private List<LatLon> pins = new List<LatLon>();
+    [SerializeField] Text longitudeText;
+    [SerializeField] Text latitudeText;
+    [SerializeField] int zoomLevelOnAddPin = 18;
 
     private void OnEnable()
     {
@@ -16,18 +26,40 @@ public class MapManager : MonoBehaviour
             Debug.Log(" BING SDK Key not found.");
             throw new System.Exception("No Key!");
         }
+
+        mapRenderer = GetComponent<MapRenderer>();
+        pinLayer = GetComponent<MapPinLayer>();
         mapSession.DeveloperKey = keys.bingMapSdkKey;
         Debug.Log("Done initialising BING SDK Key");
 
     }
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
+        // Default to 400 George st
+        longitudeText.text = "151.2073577699742";
+        latitudeText.text = "-33.868812556313266";
+
+        longitudeText.SetAllDirty();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Reset()
     {
-        
+        pinLayer.MapPins.Clear();
+        mapRenderer.ZoomLevel = 1;
+    }
+
+    public void Add()
+    {
+        var pin = Instantiate(mapPinPrefab);
+        //pin.Location = new LatLon(-33.868812556313266, 151.2073577699742); // 400 george st
+
+        Debug.Log($"Trying to add Lat:{latitudeText.text}, Long:{longitudeText.text}");
+        pin.Location = new LatLon(Convert.ToDouble(latitudeText.text), Convert.ToDouble(longitudeText.text)); // 400 george st
+        pinLayer.MapPins.Add(pin);
+        mapRenderer.Center = pin.Location;
+        mapRenderer.ZoomLevel = zoomLevelOnAddPin;
+
     }
 }
